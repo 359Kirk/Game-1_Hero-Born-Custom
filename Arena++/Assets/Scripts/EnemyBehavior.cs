761bf767d3d4cded9bffafbdbd427fc46206943a
359Kirk <brianabishopaz@gmail.com>
2022-02-23 13:01:20 -0700

    Added Basic Enemy navigation AI

--- Arena++/Assets/Scripts/EnemyBehavior.cs
+++ Arena++/Assets/Scripts/EnemyBehavior.cs
@@ -1,33 +1,92 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyBehavior : MonoBehaviour
 {
+    public Transform Player;
+    public Transform PatrolRoute;
+    public List<Transform> locations;
+
+    private int locationIndex = 0;
+    private NavMeshAgent agent;
+
+    private int _lives = 3;
+    public int EnemyLives
+    {
+        get { return _lives; }
+
+        private set
+        {
+            _lives = value;
+
+            if (_lives <= 0)
+            {
+                Destroy(this.gameObject);
+                Debug.Log("Enemy Down.");
+            }
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "Bullet(Clone)")
+        {
+            EnemyLives -= 1;
+            Debug.Log("Critical hit!");
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        Player = GameObject.Find("Player").transform;
+        InitializePatrolRoute();
+        MoveToNextPatrolLocation();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (agent.remainingDistance < 0.2f && !agent.pathPending)
+        {
+            MoveToNextPatrolLocation();
+        }
+    }
+
+    void InitializePatrolRoute()
+    {
+        foreach (Transform child in PatrolRoute)
+        {
+            locations.Add(child);
+        }
+    }
+
+    void MoveToNextPatrolLocation()
+    {
+        if (locations.Count == 0)
+            return;
+        agent.destination = locations[locationIndex].position;
+        locationIndex = (locationIndex + 1) % locations.Count;
+    }
+
     void OnTriggerEnter(Collider other)
     {
         if (other.name == "Player")
         {
+            agent.destination = Player.position;
             Debug.Log("Player Detected - attack!");
         }
     }
 
     void OnTriggerExit(Collider other)
     {
-        if(other.name == "Player")
+        if (other.name == "Player")
         {
             Debug.Log("Player out of range, resume patrol.");
         }
     }
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
--- Arena++/Assets/Scripts/GameBehavior.cs
+++ Arena++/Assets/Scripts/GameBehavior.cs
@@ -8,6 +8,7 @@ public class GameBehavior : MonoBehaviour
     public bool showWinScreen = false;
     public string labelText = "Collect all 4 items and win your freedom!";
     public int maxItems = 4;
+    public bool showLossScreen = false;
     private int _itemsCollected = 0;
     public int Items
     {
@@ -32,8 +33,25 @@ public class GameBehavior : MonoBehaviour
         set {
             _playerHP = value;
             Debug.LogFormat("Lives: {0}", _playerHP);
+            if(_playerHP <= 0)
+            {
+                labelText = "you want another life with that?";
+                showLossScreen = true;
+                Time.timeScale = 0;
+            }
+            else
+            {
+                labelText = "Ouch...that's gotta hurt.";
+            }
         }
     }
+
+    void RestartLevel()
+    {
+        SceneManager.LoadScene(0);
+        Time.timeScale = 1.0f;
+    }
+
     private void OnGUI()
     {
         GUI.Box(new Rect(20,20,150,25), "Player Health: " + _playerHP);
@@ -43,11 +61,16 @@ public class GameBehavior : MonoBehaviour
         {
             if (GUI.Button(new Rect(Screen.width / 2 - 100, Screen.height / 2 - 50, 200, 100), "YOU WON!"))
             {
-                SceneManager.LoadScene(0);
-                Time.timeScale = 1.0f;
+                RestartLevel();
             }
         }
-        
+        if (showLossScreen)
+        {
+            if(GUI.Button(new Rect(Screen.width / 2 -100, Screen.height / 2 - 50, 200, 100), "You Lose..."))
+            {
+                RestartLevel();
+            }
+        }
     }
 }
 
